4d53e146e2213f515933690773646241095786cf
chinna0107 <kancharlahemanth89@gmail.com>
2026-08-04 11:27:49 +0530

    Production 2.23

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.options('/(.*)', cors());
 app.use(express.json());
 
 const path = require('path');
